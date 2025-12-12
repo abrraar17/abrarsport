9ff409006ce16877f767f1f5a59c9812b62a612a
abrraar17 <ziamd8881@gmail.com>
2025-12-12 23:08:38 +0530

    deploy: sync envs

--- api/admin/deleteProject.js
+++ api/admin/deleteProject.js
@@ -2,9 +2,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
+
 
 export default async function handler(req, res) {
   if (req.method !== "DELETE")
